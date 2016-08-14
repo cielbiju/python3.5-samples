11d152f147c973e73b1dbb5853d4a537f4f4026b
Kyuho Kim <valve.god.project@gmail.com>
2016-08-15 01:55:21 +0900

    Create simple-web-server.py

---
+++ simple-web-server.py
@@ -0,0 +1,24 @@
+import http.server
+import socketserver
+import urllib.parse
+port = 8080
+
+class handler(http.server.BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        par = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        s = " OK {serial:"+par['serial'][0] +", user:"+par['user'][0] + " }"
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('X-ACK', s)
+        self.end_headers()
+
+        print(self.client_address)
+        print(self.requestline)
+        print(self.path)
+        
+server_address = ('', 8080)
+httpd = socketserver.TCPServer(server_address, handler)
+print("starting http server..")
+httpd.serve_forever()
